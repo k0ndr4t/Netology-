c97a037892bde7a7ca0042e565607fe7c6ec4424
k0ndr4t <130226071+k0ndr4t@users.noreply.github.com>
2023-12-03 16:42:26 +0500

    Create OperationHistoryApiApplication.java

---
+++ src/main/java/ru/netology/kondratyev_ilya/OperationHistoryApiApplication.java
@@ -0,0 +1,12 @@
+package ru.netology.kondratyev_ilya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationHistoryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperationHistoryApiApplication.class);
+    }
+}
